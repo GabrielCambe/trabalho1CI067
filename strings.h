1ab5e6b10af413a26dab082f9e75d1ea22f2af29
GabrielCambe <30998470+GabrielCambe@users.noreply.github.com>
2018-05-06 23:31:25 -0300

    removi o cabeçalho strings.h
    a alocação dentro dessa função ia ficar demasiadamente complexa, uma alocação em linha com uma macro ficou mais simples. As definições desse cabeçalho estão em "estacionamento.h"

--- strings.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdlib.h>
-
-#define MALLOC_char(nome, tamanho) nome = ((char*) malloc(sizeof(char) * tamanho)); if(nome == NULL) abort()
-#define REALLOC_char(nome, tamanho) ((char*) realloc(nome, sizeof(char) * tamanho)); if(nome == NULL) abort()
-
-
-char* nova_string();
-char* AUMENTA_string(char* STRING_pequena, int *TAMANHO);
